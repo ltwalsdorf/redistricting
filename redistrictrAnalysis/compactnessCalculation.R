fcb398b6017aee9452fa5d24cc3a80fa8fa9f567
ltwalsdorf <ltwalsdorf@gmail.com>
2025-11-12 15:04:19 -0600

    Update compactness calculation script

--- redistrictrAnalysis/compactnessCalculation.R
+++ redistrictrAnalysis/compactnessCalculation.R
@@ -1,9 +1,13 @@
 library(sf)
 library(dplyr)
 library(redistmetrics)
+library(redist)
 
-shp <- st_read("/data/illinoisExample.geojson", quiet = TRUE) |>
-  st_transform(5070)  # project to equal-area
+shp <- st_read("data/illinoisExample.geojson", quiet = TRUE) |>
+  st_transform(5070)
+
+# Build required zero-indexed adjacency for comp_edges_rem()
+adj <- redist.adjacency(shp)
 
 perims <- prep_perims(shp)
 
@@ -13,24 +17,22 @@ metrics <- tibble(
   distinct() |>
   arrange(district)
 
-add <- function(x) setNames(list(x), deparse(substitute(x)))
-
 metrics <- metrics |>
   mutate(
     comp_bc        = comp_bc(plans = shp$districtr, shp = shp),
     comp_box_reock = comp_box_reock(plans = shp$districtr, shp = shp),
     comp_ch        = comp_ch(plans = shp$districtr, shp = shp),
-    comp_edges_rem = comp_edges_rem(plans = shp$districtr, shp = shp),
-    comp_fh        = comp_fh(plans = shp$districtr, shp = shp),
-    comp_frac_kept = comp_frac_kept(plans = shp$districtr, shp = shp),
-    comp_log_st    = comp_log_st(plans = shp$districtr, shp = shp),
+    comp_edges_rem = comp_edges_rem(plans = shp$districtr, shp = shp, adj = adj),
+    # comp_fh        = comp_fh(plans = shp$districtr, shp = shp),
+    # comp_frac_kept = comp_frac_kept(plans = shp$districtr, shp = shp),
+    # comp_log_st    = comp_log_st(plans = shp$districtr, shp = shp),
     comp_lw        = comp_lw(plans = shp$districtr, shp = shp),
-    comp_polsby    = comp_polsby(plans = shp$districtr, shp = shp, perims = perims),
+    comp_polsby    = comp_polsby(plans = shp$districtr, shp = shp),
     comp_reock     = comp_reock(plans = shp$districtr, shp = shp),
     comp_schwartz  = comp_schwartz(plans = shp$districtr, shp = shp),
-    comp_skew      = comp_skew(plans = shp$districtr, shp = shp),
-    comp_x_sym     = comp_x_sym(plans = shp$districtr, shp = shp),
-    comp_y_sym     = comp_y_sym(plans = shp$districtr, shp = shp)
+    comp_skew      = comp_skew(plans = shp$districtr, shp = shp)
+    # comp_x_sym     = comp_x_sym(plans = shp$districtr, shp = shp),
+    # comp_y_sym     = comp_y_sym(plans = shp$districtr, shp = shp)
   )
 
 # Optional reference area/perimeter by district
@@ -44,4 +46,4 @@ ref <- shp |>
             perim_m = sum(.__perim_m, na.rm = TRUE), .groups = "drop")
 
 out <- left_join(metrics, ref, by = "district")
-# readr::write_csv(out, "/mnt/data/il_compactness.csv")
+readr::write_csv(out, "data/illinoisExample_compactness.csv")
